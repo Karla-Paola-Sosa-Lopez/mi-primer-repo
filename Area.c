6c488ca7e4fb53fb508c44ed27ec524d71f2970d
Karla-Paola-Sosa-Lopez <71798769+Karla-Paola-Sosa-Lopez@users.noreply.github.com>
2020-10-05 15:23:49 -0500

    Mi peresentación

---
+++ Area.c
@@ -0,0 +1,13 @@
+#include<stdio.h> // Input Output
+#include <math.h>
+
+#define PI 3.1416
+
+int main(){
+	float area,radio;
+	radio =5
+	area = PI * pow(5,2);
+	printf ("Area\n");
+	printf("Area de Circulo con Radio 5: ","%f");
+	return 0;
+}
---
+++ Presentación.c
@@ -0,0 +1,7 @@
+#include<stdio.h> //stdio -> Standard Input Output
+
+int main(){
+	printf("Hola Mundo, me llamo: ");
+	printf("Karla Paola Sosa Lopez");
+	return 0;
+}
